0b216b6cbf712e4b06a2e7140fd4226fab01551f
Fadi Asaad <firon1222@gmail.com>
2024-11-07 01:13:40 +0200

    config jest for testing and add `test` and `test:watch` npm scripts

---
+++ server/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest"
+import { pathsToModuleNameMapper } from "ts-jest"
+import { compilerOptions } from "./tsconfig.json"
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+}
+
+export default config
