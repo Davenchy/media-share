c8a8fb9af540e0b6f9c6642ec7d6f95651231928
Fadi Asaad <firon1222@gmail.com>
2024-11-09 01:23:26 +0200

    add MediaAccessGuard middleware

---
+++ server/src/middlewares/media_access_guard.ts
@@ -0,0 +1,32 @@
+import type { NextFunction, Request, Response } from "express"
+
+export type MediaAccessLevel = "OnlyOwner" | "OnlyIfPublic"
+
+/**
+ * Express middleware that checks if the user is authorized to access this
+ * media.
+ *
+ * If the user is owner, then it will pass.
+ * If the media is private or only owners are allowed, then it will block.
+ *
+ * block if -> (onlyOwner || isPrivate) && !isOwner
+ */
+export const MediaAcccessGuard =
+  (accessLevel: MediaAccessLevel) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user)
+      throw new Error("Use the MediaAccessGuard after the AuthGuard")
+    if (!req.media)
+      throw new Error("Use the MediaAccessGuard after the MediaGuard")
+
+    const isPrivate = req.media.isPrivate
+    const isOwner = req.user._id.equals(req.media._id)
+    const onlyOwner = accessLevel === "OnlyOwner"
+
+    if ((onlyOwner || isPrivate) && !isOwner) {
+      res.status(404).json({ error: "Media not found" })
+      return
+    }
+
+    next()
+  }
