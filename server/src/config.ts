46e3abc246e2661e38e829599924fa0465e6dee3
Fadi Asaad <firon1222@gmail.com>
2024-11-08 02:29:30 +0200

    update config
    - update section comment format from `// <section>` to `// <section> //`
    - update CONSOLE_LOG enviroment variable for more control and auto
    disable at production environment if not manually enabled.

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,28 +2,33 @@ import DotEnv from "dotenv"
 
 DotEnv.config()
 
-// Server Config
+// Server Config //
 export const SERVER_HOST = process.env.SERVER_HOST || "localhost"
 export const SERVER_PORT = Number(process.env.SERVER_PORT) || 3000
 
-// Mongo Database Config
+// Mongo Database Config //
 export const MONGO_DB = process.env.MONGO_DB || "media_share"
 export const MONGO_HOST = process.env.MONGO_HOST || "localhost"
 export const MONGO_PORT = Number(process.env.MONGO_PORT) || 27017
 export const MONGO_URI = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`
 
-// Environment Config
+// Environment Config //
 export const ENV = process.env.NODE_ENV || "development"
 export const IS_DEV = ENV === "development"
 export const IS_PROD = ENV === "production"
 export const IS_TEST = ENV === "test"
 
-// Logging COnfig
+// Logging COnfig //
 export const LOG_LEVEL = process.env.LOG_LEVEL || "info"
 export const LOG_FILE = process.env.LOG_FILE || "server.log"
 export const LOG_EXCEPTIONS_FILE = process.env.LOG_FILE || "exceptions.log"
 export const LOG_ERROR_FILE = process.env.LOG_ERROR_FILE || "error.log"
-export const LOG_CONSOLE = process.env.LOG_CONSOLE !== undefined || !IS_PROD
+
+const logConsole = process.env.LOG_CONSOLE
+// if LOG_CONSOLE is not set, only print logs to stdout if not in production environment
+// if LOG_CONSOLE is set, always print logs to stdout if its value is `enabled`
+export const LOG_CONSOLE =
+  logConsole === undefined ? !IS_PROD : logConsole === "enabled"
 
 // Password Hasing Config //
 export const PASSWORD_SALT_ROUNDS =
