3e35bbf7667173fabf58d661d4956e34fea6eaf6
Fadi Asaad <firon1222@gmail.com>
2024-11-09 01:24:51 +0200

    add The Media router

---
+++ server/src/routes/MediaRouter.ts
@@ -0,0 +1,41 @@
+import { Router } from "express"
+import MediaController from "controller/MediaController"
+import { MediaGuard } from "@/middlewares/media_guard"
+import { MediaAcccessGuard } from "@/middlewares/media_access_guard"
+import { UploadArray } from "@/middlewares/file_upload"
+import { FindMediaLike } from "@/middlewares/find_media_like"
+
+const router = Router()
+
+router.post("/", UploadArray, MediaController.upload)
+router.get("/", MediaController.allMedia)
+
+router.get(
+  "/:mediaId",
+  MediaGuard,
+  MediaAcccessGuard("OnlyIfPublic"),
+  FindMediaLike,
+  MediaController.metadata,
+)
+router.patch(
+  "/:mediaId",
+  MediaGuard,
+  MediaAcccessGuard("OnlyOwner"),
+  MediaController.update,
+)
+router.delete(
+  "/:mediaId",
+  MediaGuard,
+  MediaAcccessGuard("OnlyOwner"),
+  FindMediaLike,
+  MediaController.delete,
+)
+
+router.get(
+  "/:mediaId/stream",
+  MediaGuard,
+  MediaAcccessGuard("OnlyIfPublic"),
+  MediaController.stream,
+)
+
+export default router
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,11 @@
+import { AuthGuard } from "@/middlewares/auth_guard"
 import { Router } from "express"
+import MediaRouter from "./MediaRouter"
 import UsersRouter from "./UsersRouter"
 
 const router = Router()
 
 router.use(UsersRouter)
+router.use("/media", AuthGuard, MediaRouter)
 
 export default router
