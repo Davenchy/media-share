ded488bbf3a78847177cf67cb92a4f6dfd4c6dd2
Fadi Asaad <firon1222@gmail.com>
2024-11-08 02:36:02 +0200

    resort imports in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
-import express from "express"
-import mongoose from "mongoose"
 import { IS_TEST, MONGO_URI, SERVER_HOST, SERVER_PORT } from "@/config"
 import api_router from "@/routes"
-import logger from "./utils/logger"
+import express from "express"
+import mongoose from "mongoose"
 import { ErrorHandler, NotFoundHandler } from "./middlewares/error_handler"
+import logger from "./utils/logger"
 
 const app = express()
 
@@ -13,6 +13,7 @@ app.use(api_router)
 
 app.use(NotFoundHandler)
 app.use(ErrorHandler)
+
 async function run_server() {
   logger.info("connecting to MongoDB")
   await mongoose.connect(MONGO_URI)
