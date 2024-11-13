69e19429b4e267ef7981b9c57db078f71b96d5c0
Fadi Asaad <firon1222@gmail.com>
2024-11-13 03:10:32 +0200

    fix MediaLikes routes naming

--- server/src/routes/MediaLikesRouter.ts
+++ server/src/routes/MediaLikesRouter.ts
@@ -1,11 +1,11 @@
-import { Router } from "express"
 import MediaLikesController from "controller/MediaLikesController"
+import { Router } from "express"
 import { FindMediaLike } from "middleware/find_media_like"
 
 const router = Router()
 
-router.get("/likes", MediaLikesController.getAllLikes)
-router.post("/likes", FindMediaLike, MediaLikesController.like)
-router.delete("/likes", FindMediaLike, MediaLikesController.unlike)
+router.get("/", MediaLikesController.getAllMediaLikes)
+router.post("/", FindMediaLike, MediaLikesController.like)
+router.delete("/", FindMediaLike, MediaLikesController.unlike)
 
 export default router
