e5c3a3da541b60c025192e0cd5a9fbb033ea1973
Fadi Asaad <firon1222@gmail.com>
2024-11-13 04:09:38 +0200

    update the app component to handle different authentication states

--- web_client/src/App.tsx
+++ web_client/src/App.tsx
@@ -1,4 +1,13 @@
+import { useUser } from "./hooks/use-user"
 function App() {
+  const { isLoggedIn, isLoggedOut, isChecking, restoreSession, state } =
+    useUser()
+
+  // try to restore the last session if possible otherwise logout
+  if (state === "IDLE") restoreSession()
+
+  // Loading screen
+  if (isChecking) {
     return (
       <div className="place-content-center h-lvh px-2">
         <div className="md:max-w-screen-sm mx-auto space-y-8">
@@ -8,6 +17,30 @@ function App() {
         </div>
       </div>
     )
+  }
+
+  if (isLoggedIn) {
+
+  const removeLocalStorage = () => {
+    localStorage.clear()
+    location.reload()
+  }
+
+  return (
+    <div className="place-content-center h-lvh px-2">
+      <div className="md:max-w-screen-sm mx-auto p-4 rounded space-y-8 bg-primary-foreground">
+        <p className="text-red-600 text-3xl md:text-6xl font-bold text-center">
+          Error
+        </p>
+        <p className="text-destructive text-center">
+          Something went wrong, try to remove the app cache, then try again.
+        </p>
+        <div className="flex flex-row-reverse">
+          <Button onClick={removeLocalStorage}>Clear Cached Data</Button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
