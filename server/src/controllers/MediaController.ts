c043fd19e4a8d5f3a80adfeb03ff719082b18bb4
Fadi Asaad <firon1222@gmail.com>
2024-11-13 03:24:36 +0200

    implement the update media endpoint `PUT /media/:mediaId`

--- server/src/controllers/MediaController.ts
+++ server/src/controllers/MediaController.ts
@@ -1,9 +1,17 @@
+import { AsyncHandler } from "@/decorators/async_error_handler"
 class MediaController {
   upload() {}
 
   metadata() {}
+  @AsyncHandler
+  async update(req: Request, res: Response) {
+    const { media, body } = req
+    if (!media) throw new Error("Use MediaGuard")
+    await media.updateOne({ $set: body })
+    res.send()
+  }
 
-  update() {}
+
 
   delete() {}
 
